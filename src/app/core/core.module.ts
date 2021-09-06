aa572d6872db8a693fe182ea0990a7a57db940cf
HancharykNastassia <goncharik.anastasia@mail.ru>
2021-09-06 14:38:24 +0300

    feat: add menue as contacts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { LocationComponent } from './components/location/location.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ContactsComponent, LocationComponent],
   imports: [CommonModule, SharedModule],
+  exports: [ContactsComponent],
 })
 export class CoreModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, MatExpansionModule],
+  exports: [MatButtonModule, MatExpansionModule, MatMenuModule],
 })
 export class SharedModule {}
