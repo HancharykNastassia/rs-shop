ddfe1b3a5ffe64a4445fb0eb8d307272cf8ac69e
HancharykNastassia <goncharik.anastasia@mail.ru>
2021-09-19 21:03:14 +0300

    feat: add account button menu

--- src/app/core/components/category-nav-block/category-nav-block.component.ts
+++ src/app/core/components/category-nav-block/category-nav-block.component.ts
@@ -1,10 +1,9 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { getCategories } from 'src/app/redux/actions/categories-actions';
+import { map } from 'rxjs/operators';
 import { AppState } from 'src/app/redux/state.models';
 import { CategoryModel, SubcategoryModel } from '../../models/category-models';
 import { GoodsService } from '../../services/goods.service';
@@ -22,7 +21,6 @@ export class CategoryNavBlockComponent implements OnInit {
   constructor(private categoryService: GoodsService, private store: Store<AppState>, private router: Router) { }
 
   ngOnInit(): void {
-    this.store.dispatch(getCategories());
     this.categories$ = this.store.select((state) => state.categories.categories);
   }
 
--- src/app/core/components/nav-block/nav-block.component.ts
+++ src/app/core/components/nav-block/nav-block.component.ts
@@ -1,13 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/redux/state.models';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-nav-block',
   templateUrl: './nav-block.component.html',
   styleUrls: ['./nav-block.component.scss']
 })
-export class NavBlockComponent{
+export class NavBlockComponent implements OnInit{
   @Output() onCatalogBtnClick = new EventEmitter<any>();
   @Input() isCatalogOpen = false;
+  @Input() user$!: Observable<User | undefined>
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.user$ = this.store.select((state) => state.user.user);
+  }
 
   toggleCatalog(){
     this.isCatalogOpen = !this.isCatalogOpen;
