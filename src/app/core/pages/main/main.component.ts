3010f0622528a41d8f4ff868091a680844a53b32
HancharykNastassia <goncharik.anastasia@mail.ru>
2021-09-23 16:17:36 +0300

    feat: add first slider

--- src/app/core/pages/main/main.component.ts
+++ src/app/core/pages/main/main.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { forkJoin, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { AppState } from 'src/app/redux/state.models';
+import { ItemModel } from '../../models/item-models';
+import { GoodsService } from '../../services/goods.service';
 
 @Component({
   selector: 'app-main',
@@ -6,7 +13,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  constructor() {}
+  @Input() firstSlides$!: Observable<ItemModel[]>;
 
-  ngOnInit(): void {}
+  firstOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<', '>'],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 1,
+      },
+      740: {
+        items: 1,
+      },
+      940: {
+        items: 1,
+      },
+    },
+    nav: true,
+  };
+  constructor(private store: Store<AppState>, private dataService: GoodsService) {}
+
+  ngOnInit(): void {
+    this.firstSlides$ = this.store.select((state) => state.categories.categories).pipe(
+      switchMap(cats => forkJoin(cats.map(cat => this.dataService.getGoodsFrom(cat.id, undefined, undefined, 2).pipe(
+        map(item => item[0].imageUrls[0] ? item[0] : item[1])
+      ))))
+    )
+  }
 }
